0270376bcc44f3200c5ffeff9dd96acc90f0a26b
zahrasheykhlar <zahra.sheykhlar@gmail.com>
2020-06-11 05:56:44 +0430

    Decoded Token Fix!

--- services/auth0.js
+++ services/auth0.js
@@ -69,6 +69,7 @@ class Auth0 {
 		if(token)
 		{
 			const decodedToken = jwt.decode(token, {complete: true});
+			if(!decodedToken){return undefined;}
 			const jwks = await this.getJWKS();
 			const jwk = jwks.keys[0];
 			
@@ -76,9 +77,7 @@ class Auth0 {
 			let cert = jwk.x5c[0];
 			cert = cert.match(/.{1,64}/g).join('\n');
 			cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
-			//
-console.log('jwk', jwk.kid);
-console.log('decodedToken', decodedToken.header.kid);
+	
 			if (jwk.kid === decodedToken.header.kid){
 				try{
 					const verifiedToken = jwt.verify(token, cert)
