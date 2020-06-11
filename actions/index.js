4f61f4b73db143544a1f6d8ceec1097fcc74bd09
zahrasheykhlar <zahra.sheykhlar@gmail.com>
2020-06-12 01:42:43 +0430

    Axios Auth Headers

---
+++ actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const setAuthHeader = () => {
+	const token = Cookies.getJSON('jwt');
+	if(token)
+	{
+		return {headers: {'authorization': `Bearer ${Cookies.getJSON('jwt')}`}}
+	}
+	return undefined;
+	
+}
+export const getSecretData = async () =>{
+	return await axios.get('/api/v1/secret', setAuthHeader()).then((response) => response.data);
+
+}
--- pages/secret.js
+++ pages/secret.js
@@ -3,7 +3,7 @@ import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
 
-import axios from 'axios'
+import {getSecretData} from '../actions';
 
 class Secret extends React.Component {
 
@@ -20,8 +20,8 @@ constructor()
 }
 async componentDidMount()
 {
-	const res = await axios.get('/api/v1/secret');
-	const secretData = res.data;
+	const secretData = await getSecretData();
+	
 	this.setState({
 		secretData
 	});
