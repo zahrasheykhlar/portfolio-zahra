367f13e2ab95753c4a69036f14791ea2d345d9b7
zahrasheykhlar <zahra.sheykhlar@gmail.com>
2020-06-17 03:52:49 +0430

    Card Styling

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -3,6 +3,7 @@ import BaseLayout from '../components/layouts/BaseLayout';
 import axios from 'axios';
 import Link from 'next/link';
 import BasePage from '../components/BasePage';
+import {Col, Row, Card, CardHeader, CardBody, CardText, CardTitle} from 'reactstrap';
 
 class Portfolios extends React.Component {
 static async getInitialProps()
@@ -25,11 +26,22 @@ static async getInitialProps()
 	{
 		return  posts.map((post, index) => {
 			return (
-					<li key={index}>
-				    	<Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
-	        				<a style={{'fontSize':'30px'}}> {post.title} </a>
-	     				</Link>
-					</li>
+				<Col md="4">
+  						<React.Fragment key={index}>
+  						  <span>
+ 						     <Card className="portfolio-card">
+  						      <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+     						   <CardBody>
+     						     <p className="portfolio-card-city"> Some Location {index} </p>
+     						     <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+     						     <CardText className="portfolio-card-text">Some Description {index}</CardText>
+     						     <div className="readMore"> </div>
+     						   </CardBody>
+     						 </Card>
+   						 </span>
+  						</React.Fragment>
+				</Col>
+		
 					)
 		})
 	}
@@ -38,10 +50,10 @@ static async getInitialProps()
   	const { posts } = this.props;
     return (
     	<BaseLayout {...this.props.auth}>
-	    	<BasePage title="Portfolios">
-		    	<ul>
+	    	<BasePage className="portfolio-page" title="Portfolios">
+		    	<Row>
 		    		{this.renderPosts(posts)}
-		    	</ul>
+		    	</Row>
 		    </BasePage>
 	    </BaseLayout>
     	)
