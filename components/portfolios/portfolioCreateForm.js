eaad6e29c3886b541beb1c51080c6ea96b2c31e1
zahrasheykhlar <zahra.sheykhlar@gmail.com>
2020-06-17 07:37:18 +0430

    More Input Types

--- components/portfolios/portfolioCreateForm.js
+++ components/portfolios/portfolioCreateForm.js
@@ -3,18 +3,20 @@ import React from 'react';
 export default class PortfolioCreateForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {title: '', description: '', language:''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+  const field = event.target.name;
+    this.setState({[field]: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.title + ' '+ this.state.description + ' '+ this.state.language);
     event.preventDefault();
   }
 
@@ -23,7 +25,20 @@ export default class PortfolioCreateForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input name="title" type="text" value={this.state.title} onChange={this.handleChange} />
+        </label>
+              <label>
+          Description:
+          <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+        </label>
+         <label>
+          Pick your favorite programming language:
+          <select name="language" value={this.state.language} onChange={this.handleChange}>
+            <option value="javascript">Javascript</option>
+            <option value="java">Java</option>
+            <option value="c++">C++</option>
+            <option value="c#">C#</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
